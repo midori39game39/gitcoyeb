78e7652413573c9a03e444f93679fb18df1020e9
midori39game39 <midori39game@gmail.com>
2026-05-24 05:25:37 +0900

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -655,10 +655,10 @@ async def log(interaction: discord.Interaction):
 async def on_ready():
     print(f'起動: {client.user}')
 
-    channel = client.get_channel(1260454361297850432)
+    channel = client.get_channel(1504126531256061982)
 
     # 既存のEmbedを全部削除
-    async for message in channel.history(limit=None):
+    async for message in channel.history(limit=20):
         if message.author == client.user and message.embeds:
             await message.delete()
 
